1fd878a7a582c903d5d5ce313e536c9c9c50736b
Ilya Efimov <ilya02282000@gmail.com>
2020-01-27 15:50:50 +0300

    Task 2 has arrived

--- lab1/lab1_main.cs
+++ lab1/lab1_main.cs
@@ -1,42 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace lab1
 {
-	public class A
-	{
-		
-	}
+	public class LinEquation {
+		protected List<double> x;
 
-	public class B : A
-	{
-		protected Object o1;
+		protected List<double> calcLinEquation(double a, double b) {
+			List<double> t = new List<double>(); 
 
-		public B(Object o1)
-		{
-			this.o1 = o1;
+			if (a == 0 || b == 0) throw new Exception(":(");
+			t.Add(b / a);
+			return t;
 		}
 	}
 
-	public class C : B
-	{
-		protected Object o2, o3;
-	
-		public C(Object o1, Object o2, Object o3)
-		{
-			this.o1 = o1;
-			this.o2 = o2;
-			this.o3 = o3;
+	public class QuadEquation : LinEquation {
+		public QuadEquation() {}
+
+		protected double calcDiscriminant(double a, double b, double c) {			
+			return (Math.Pow(b, 2) - 4 * a * c);
+		}
+
+		//ax^2 + bx + c = 0
+		public List<double> calcQuadEquation(double a, double b, double c) {
+			List<double> t = new List<double>();
+			if (a == 0) {
+				return (calcLinEquation(b, c));
+			}
+
+			double d = calcDiscriminant (a, b, c);
+			if (d < 0) {
+				throw new Exception(":(");
+			}
+			else if(d == 0) {
+				t.Add (-b / (2 * a));
+				return t;
+			}
+
+			else {
+				t.Add(((-b + Math.Sqrt(d))/(2 * a)));
+				t.Add(((-b - Math.Sqrt(d))/(2 * a)));
+				return t;
+			}
 		}
 	}
 
-	public class Main
-	{
-		Object a1 = new A();
-		Object a2 = new A();
-		Object a3 = new A();
-		Object b1 = new B(a2);
-		Object c1 = new C(b1, a1, a2);
+	public class Program {		
+		static void Main() {
+			QuadEquation exp = new QuadEquation ();
+			List<double> result = new List<double>();
 
+			try {
+				result = exp.calcQuadEquation(0, 0, 0);
+			}
+			catch(Exception e) {
+				Console.WriteLine (e.Message);
+			}
+
+			if (result.Count == 2)
+				Console.WriteLine ("X1 = " + result [0] + ", X2 = " + result [1]);
+			else if (result.Count == 1)
+				Console.WriteLine ("X = " + result [0]);
+		}
 	}
 
 }
